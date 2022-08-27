f7d86d14dedc38a74fca0afb87a653d01df783a5
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-27 19:12:46 +0300

    feat: extract logic from chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import Chart from './components/Chart'
+import { ETHTransaction } from './types/ETHTransaction'
+import { NetworkType } from './types/NetworkType'
 import { Timeframe } from './types/Timeframe'
+import { filterDay, filterWeek } from './utils/ETHTransactionFilters'
+import gasPriceData from './json/gas_price.json'
 
 const App: FC = () => {
   const [timeframe, setTimeframe] = useState<Timeframe>(Timeframe.H1)
+  const [networkType] = useState<NetworkType>(NetworkType.ETHEREUM)
+  const [networkData] = useState(gasPriceData)
+  const [chartData, setChartData] = useState<ETHTransaction[]>(
+    networkData[networkType].transactions.slice(0, 200),
+  )
+
+  useEffect(() => {
+    let data: ETHTransaction[] = []
+
+    if (timeframe === Timeframe.H1) {
+      data = networkData[networkType].transactions.slice(0, 200)
+    } else if (timeframe === Timeframe.D1) {
+      data = networkData[networkType].transactions.filter(filterDay)
+    } else if (timeframe === Timeframe.W1) {
+      data = networkData[networkType].transactions.filter(filterWeek)
+    }
+
+    setChartData(data)
+  }, [networkData, timeframe, networkType])
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4">
@@ -21,7 +44,7 @@ const App: FC = () => {
         </select>
       </div>
 
-      <Chart timeframe={timeframe} />
+      <Chart chartData={chartData} />
     </div>
   )
 }
--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,36 +1,13 @@
-import { FC, useState, useEffect } from 'react'
+import { FC } from 'react'
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Brush } from 'recharts'
-import gasPriceData from '../../json/gas_price.json'
 import { timeFormatter } from '../../utils/timeFormatter'
-import { filterDay, filterWeek } from '../../utils/ETHTransactionFilters'
-import { NetworkType } from '../../types/NetworkType'
 import { ETHTransaction } from '../../types/ETHTransaction'
-import { Timeframe } from '../../types/Timeframe'
 
 interface ChartProps {
-  timeframe: Timeframe
+  chartData: ETHTransaction[]
 }
 
-const Chart: FC<ChartProps> = ({ timeframe }) => {
-  const [networkData] = useState(gasPriceData)
-  const [chartData, setChartData] = useState<ETHTransaction[]>(
-    networkData[NetworkType.ETHEREUM].transactions.slice(0, 200),
-  )
-
-  useEffect(() => {
-    let data: ETHTransaction[] = []
-
-    if (timeframe === Timeframe.H1) {
-      data = networkData[NetworkType.ETHEREUM].transactions.slice(0, 200)
-    } else if (timeframe === Timeframe.D1) {
-      data = networkData[NetworkType.ETHEREUM].transactions.filter(filterDay)
-    } else if (timeframe === Timeframe.W1) {
-      data = networkData[NetworkType.ETHEREUM].transactions.filter(filterWeek)
-    }
-
-    setChartData(data)
-  }, [networkData, timeframe])
-
+const Chart: FC<ChartProps> = ({ chartData }) => {
   return (
     <div>
       <LineChart width={600} height={400} data={chartData}>
